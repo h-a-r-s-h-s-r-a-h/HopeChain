5fda0173e3a7e5b8f59d1ce113fc909a1f8a8e19
Harsh <harshvirat894@gmail.com>
2025-04-18 01:17:51 +0530

    anchor

--- anchor-hope-chain/programs/anchor-hope-chain/src/state/donator.rs
+++ anchor-hope-chain/programs/anchor-hope-chain/src/state/donator.rs
@@ -1,4 +1,13 @@
 use anchor_lang::prelude::*;
 
 #[account]
-pub struct DonatorAccountState {}
+pub struct DonatorAccountState {
+    pub donator_pubkey: PubKey,
+    pub fund_id: String,
+    pub blessing_message: String,
+    pub donator_name: String,
+    pub total_donated_amount: u32,
+    pub email_of_donator: String,
+    pub phone_of_donator: String,
+    pub funded_at: i64,
+}
--- anchor-hope-chain/programs/anchor-hope-chain/src/state/donee.rs
+++ anchor-hope-chain/programs/anchor-hope-chain/src/state/donee.rs
@@ -2,12 +2,16 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct DoneeAccountState {
+    pub fund_id: String,
     pub fund_generator: Pubkey,
     pub accountNo: PubKey,
     pub name_of_fund_raiser: String,
     pub aadhar_no_of_fund_raiser: u64,
     pub description_of_problem: String,
-    pub target: u32,
+    pub email_of_fundraiser: String,
+    pub phone_of_fundraiser: String,
+    pub total_amount_needed: u32,
+    pub total_amount_collected: u32,
     pub created_at: i64,
     pub is_active: bool,
 }
